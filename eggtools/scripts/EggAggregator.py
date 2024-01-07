e8d736ee4a7777b16729fea9f0aeec7841abc820
Erica M. ("Loonatic") <l.oony@aol.com>
2024-01-07 16:34:15 -0600

    EggAggregator: Finish script

--- eggtools/scripts/EggAggregator.py
+++ eggtools/scripts/EggAggregator.py
@@ -4,7 +4,9 @@ egg-aggregate <base file> [eggs to aggregate into base] -o <output file>
 
 """
 import argparse
+import copy
 import os
+import sys
 
 from panda3d.core import Filename
 
@@ -13,7 +15,7 @@ from eggtools.EggMan import EggMan
 parser = argparse.ArgumentParser(
     prog='egg-aggregate',
     epilog='Aggregates egg files into one.',
-    description='egg-aggregate input.egg [eggs to aggregate into input] -o output.egg'
+    description='python -m eggtools.scripts.EggAggregator input.egg [eggs to aggregate into input] -o output.egg'
 )
 parser.add_argument(
     'input_egg',
@@ -37,17 +39,36 @@ parser.add_argument(
     help='If this option is given, the input egg files will be rewritten in place with the results.',
 )
 
-
 args = parser.parse_args()
-print(args)
-print(args.output)
+input_loc = args.input_egg
+if not input_loc:
+    print("Error: You must specify an input egg file!")
+    sys.exit()
 
-print(f"{args.input_egg} | {args.other_egg_filepaths}")
+if not args.other_egg_filepaths:
+    print("Error: You must specify at least one other egg file to aggregate!")
 
-input_egg = Filename.fromOsSpecific(os.path.join(os.getcwd(), args.input_egg))
+input_egg = Filename.fromOsSpecific(input_loc)
+
+output_loc = args.output
+if not args.output and args.inplace:
+    output_loc = os.path.join(os.getcwd(), args.input_egg)
+if not output_loc:
+    print("Error: You must either specify an output filepath with -o or use --inplace.")
+    sys.exit()
+output_file = Filename.fromOsSpecific(os.path.join(os.getcwd(), output_loc))
+
 other_eggs = args.other_egg_filepaths
-all_eggs = other_eggs.insert(0, input_egg)
-
+all_eggs = copy.deepcopy(other_eggs)
+all_eggs.insert(0, input_egg)
 eggman = EggMan(all_eggs)
+
+other_egg_datas = []
+for egg_name in other_eggs:
+    other_egg_datas.append(eggman.get_egg_by_filename(egg_name))
+
 _src_egg = eggman.get_egg_by_filename(input_egg)
-print(_src_egg.getName())
+eggman.merge_eggs(_src_egg, other_egg_datas)
+
+eggman.write_egg(_src_egg, output_file)
+print(f"Aggregated {len(other_egg_datas) + 1} egg files to {output_file.toOsSpecific()}")
