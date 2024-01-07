98eeba6f86e81e71a63f48db73cc7b279b27378a
Erica M. ("Loonatic") <l.oony@aol.com>
2024-01-07 14:27:51 -0600

    Tests: Test attrs module

---
+++ tests/test_attrs.py
@@ -0,0 +1,33 @@
+from eggtools.attributes.EggAlphaAttribute import EggAlpha, EggAlphaAttribute
+from eggtools.attributes.EggBackstageAttribute import EggBackstage, EggBackstageAttribute
+from eggtools.attributes.EggBillboardAttribute import EggBillboard, EggBillboardAttribute
+from eggtools.attributes.EggBinAttribute import EggBin, EggBinAttribute
+from eggtools.attributes.EggCollideAttribute import EggCollide, EggCollideAttribute
+from eggtools.attributes.EggCollideMaskAttribute import EggCollideMask, EggCollideMaskAttribute
+from eggtools.attributes.EggDCSAttribute import EggDCS, EggDCSAttribute
+from eggtools.attributes.EggDartAttribute import EggDart, EggDartAttribute
+from eggtools.attributes.EggDecalAttribute import EggDecal, EggDecalAttribute
+from eggtools.attributes.EggModelAttribute import EggModel, EggModelAttribute
+from eggtools.attributes.EggSequenceAttribute import EggSequence, EggSequenceAttribute
+from eggtools.attributes.EggTagAttribute import EggTag, EggTagAttribute
+from eggtools.attributes.EggExtFileAttribute import EggExtFile, EggExtFileAttribute
+
+
+def test_egg_attrs():
+    EggAlphaAttribute("Dual")
+    EggBackstageAttribute()
+    EggBillboardAttribute("axis")
+    EggBinAttribute("ground")
+    EggCollideAttribute('none', 'none')
+    EggCollideMaskAttribute(value='from')
+    EggDCSAttribute("net")
+    EggDartAttribute("structured")
+    EggDecalAttribute()
+    EggExtFileAttribute("dummy.egg")
+    EggModelAttribute()
+    EggSequenceAttribute()
+    EggTagAttribute("key", "val")
+
+
+if __name__ == "__main__":
+    test_egg_attrs()
