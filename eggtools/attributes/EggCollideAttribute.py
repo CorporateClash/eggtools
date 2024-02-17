c1a49eb77eebfc3bdea5a9318bc65f127a2108e6
Erica M. ("Loonatic") <l.oony@aol.com>
2024-02-17 00:08:17 -0600

    Documentation: Add documentation

--- eggtools/attributes/EggCollideAttribute.py
+++ eggtools/attributes/EggCollideAttribute.py
@@ -2,29 +2,79 @@ from panda3d.egg import EggGroup
 
 from eggtools.attributes.EggAttribute import EggAttribute
 
+# Collision Solids
 cs2type = {
     'none': EggGroup.CST_none,
+
+    # The geometry represents an infinite plane.
+    # The first polygon found in the group will define the plane.
     'plane': EggGroup.CST_plane,
+
+    # The geometry represents a single polygon.
+    # The first polygon is used.
     'polygon': EggGroup.CST_polygon,
+
+    # The geometry represents a complex shape made up of several polygons.
+    # This collision type should not be overused, as it provides the least optimization benefit.
     'polyset': EggGroup.CST_polyset,
+
+    # The geometry represents a sphere.
+    # The vertices in the group are averaged together to determine the sphere’s center and radius.
     'sphere': EggGroup.CST_sphere,
+
+    # The geometry represents a tube.
+    # This is a cylinder-like shape with hemispherical endcaps;
+    # it is sometimes called a capsule or a lozenge in other packages.
+    # The smallest tube shape that will fit around the vertices is used.
     'tube': EggGroup.CST_tube,
+
+    # The geometry represents an inverse sphere.
+    # This is the same as Sphere, with the normal inverted, so that the solid part of an inverse sphere is the
+    # entire world outside of it.
+    # Note that an inverse sphere is in infinitely large solid with a finite hole cut into it.
     'inv_sphere': EggGroup.CST_inv_sphere,
     'invsphere': EggGroup.CST_inv_sphere,
-    'box': EggGroup.CST_box,
-    'floor_mesh': EggGroup.CST_floor_mesh,
 
+    # The geometry represents a box.
+    # The smallest axis-alligned box that will fit around the vertices is used.
+    'box': EggGroup.CST_box,
+
+    'floor_mesh': EggGroup.CST_floor_mesh,
 }
+
+# Collision Flags
 
 flags2type = {
     'none': EggGroup.CF_none,
+
+    # Each group descended from this node that contains geometry will define a new collision object of the given type.
+    # The event name, if any, will also be inherited from the top node and shared among all the collision objects.
+    # This option will soon be the default; it is suggested that it is always specified for most compatibility.
     'descend': EggGroup.CF_descend,
+
+    # Throws the name of the <Collide> entry, or the name of the surface if the <Collide> entry has no name,
+    # as an event whenever an avatar strikes the solid.
+    # This is the default if the <Collide> entry has a name.
     'event': EggGroup.CF_event,
+
+    # Don’t discard the visible geometry after using it to define a collision surface;
+    # create both an invisible collision surface and the visible geometry.
     'keep': EggGroup.CF_keep,
+
     'solid': EggGroup.CF_solid,
+
     'center': EggGroup.CF_center,
+
     'turnstile': EggGroup.CF_turnstile,
+
+    # Stores a special effective normal with the collision solid that points up,
+    # regardless of the actual shape or orientation of the solid.
+    # This can be used to allow an avatar to stand on a sloping surface without having a tendency to slide downward.
     'level': EggGroup.CF_level,
+
+    # Rather than being a solid collision surface, the defined surface represents a boundary.
+    # The name of the surface will be thrown as an event when an avatar crosses into the interior,
+    # and name-out will be thrown when an avatar exits.
     'intangible': EggGroup.CF_intangible,
 
 }
--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -10,7 +10,12 @@ if not os.path.isfile('tests/test_tiles.egg'):
 else:
     test_egg = Filename.fromOsSpecific(os.path.join(os.getcwd(), 'tests/test_tiles.egg'))
 
+# Initialize an empty EggMan instance
 eggman = EggMan([])
+
+# Generate an arbitrary EggData instance outside the EggMan
 eggdata = EggData()
 eggdata.read(test_egg)
+
+# Try to read an EggData that isn't initialized with our EggMan. This should cause an exception.
 eggman.apply_attributes(eggdata)
