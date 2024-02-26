129a5d4e278575dc1532b9de49d9a808a9dccaed
Erica M. ("Loonatic") <l.oony@aol.com>
2024-02-26 01:17:49 -0600

    FlattenTransform: Ignore billboarded nodes by default

--- eggtools/attributes/EggFlattenTransformAttribute.py
+++ eggtools/attributes/EggFlattenTransformAttribute.py
@@ -1,3 +1,5 @@
+from panda3d.egg import EggVertexPool
+
 from eggtools.attributes.EggAttribute import EggAttribute
 
 
@@ -12,9 +14,10 @@ class EggFlattenTransformAttribute(EggAttribute):
     it may be a good idea to call remove_unused_vertices() after calling this.
     """
 
-    def __init__(self):
+    def __init__(self, flatten_billboards: bool = False, flatten_dcs: bool = False):
         # Not a real attribute
         super().__init__("Modifier", "flatten-transforms", "True")
+        self.flatten_billboards = flatten_billboards
 
     def _modify_group(self, egg_group):
         pass
@@ -24,9 +27,15 @@ class EggFlattenTransformAttribute(EggAttribute):
 
     def _modify_node(self, egg_node):
         if self.target_nodes.check(egg_node.getName()) and hasattr(egg_node, "flatten_transforms"):
+            if isinstance(egg_node, EggVertexPool):
+                target = egg_node.parent
+            else:
+                target = egg_node
+            if target.billboard_type and not self.flatten_billboards:
+                return
             egg_node.flatten_transforms()
 
 
 class EggFlattenTransform(EggFlattenTransformAttribute):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, flatten_billboards: bool = False, flatten_dcs: bool = False):
+        super().__init__(flatten_billboards, flatten_dcs)
